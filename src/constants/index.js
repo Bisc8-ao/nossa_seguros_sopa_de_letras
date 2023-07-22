1da470856f15929a844978c9be37f9096b7d2abf
tchiinhemba <tchiinhemba@gmail.com>
2023-07-22 14:16:51 +0100

    add all constants

---
+++ src/constants/index.js
@@ -0,0 +1,9 @@
+export const LEVEL_SETTINGS_MAP = [
+    { texto_botao: "Seleccione...", tam_board: 10, area_board: 100, tempo_jogo: 600, num_palavras: 0, pontos_palavra: 10, modo_hardcore: false },
+    { texto_botao: "Fácil", tam_board: 10, area_board: 100, tempo_jogo: 60, num_palavras: 9, pontos_palavra: 10, modo_hardcore: false },
+    { texto_botao: "Médio", tam_board: 20, area_board: 400, tempo_jogo: 450, num_palavras: 10, pontos_palavra: 20, modo_hardcore: false },
+    { texto_botao: "Difícil", tam_board: 30, area_board: 900, tempo_jogo: 300, num_palavras: 15, pontos_palavra: 30, modo_hardcore: true }
+];
+export const ABCD = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'].sort(() => Math.random() - 0.5);
+export let LOWERWORDS = ["multirisco", "habitacao", "saude", "vida", "atomovel", "apolice", "nossa", "seguros", "viagem"];
+export let WORDS = ["MULTIRISCO", "HABITACAO", "SAUDE", "VIDA", "AUTOMOVEL", "APOLICE", "NOSSA", "SEGUROS", "VIAGEM"];
